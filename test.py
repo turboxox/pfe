817833c377cc47c6ba4065d8b7580a79119c22bf
turboxox <ismail.saadr@gmail.com>
2026-01-28 07:19:20 +0100

    creat flows and u should add timeout for 5 sec in each flow

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from scapy.all import *
 
 
-p=sniff(filter="udp", count=1, iface="Intel(R) Wi-Fi 6 AX201 160MHz")
-p[0].show()
+interface_name="Intel(R) Wi-Fi 6 AX201 160MHz"
+
+def show_ip(packet):
+    if packet.haslayer(UDP):
+        print(f"Source: {packet[IP].src} -> Dest: {packet[IP].dst}")
+
+sniff(filter="UDP", prn=show_ip, count=10)
--- try.py
+++ try.py
@@ -1,25 +1,40 @@
 from scapy.all import *
+from datetime import datetime
 
 
+flows ={}
+flow_timeout = 5
 interface_name="Intel(R) Wi-Fi 6 AX201 160MHz"
 def filter_packet(pkt):
-    
-    timestamp = pkt.time
-    readable_time = datetime.fromtimestamp(timestamp)
-    
-    print(f"\n{'='*60}")
-    print(f"Timestamp: {readable_time}")
-    print(f"Raw time: {timestamp}")
-    
+    timestamp = pkt.time   
     if pkt.haslayer('IP'):  
-        print("src:", pkt['IP'].src,  
-          "dst:", pkt['IP'].dst,
-          "Protocol:", pkt['IP'].proto,
-          "Total IP packet length:", pkt['IP'].len)  
-    
-    if pkt.haslayer('UDP'): 
-        print("Source port:", pkt['UDP'].sport,
-              "Destination port:", pkt['UDP'].dport,
-              "UDP payload length:", pkt['UDP'].len)
+        src=pkt['IP'].src
+        dst =pkt['IP'].dst
+        protocol= pkt['IP'].proto
+        tipl =pkt['IP'].len
 
-sniff(iface=interface_name , prn=filter_packet)
+        if pkt.haslayer('UDP'): 
+            sport =pkt['UDP'].sport
+            dport=pkt['UDP'].dport
+            proto_name ="UDP"        
+            flow_key=(src , dst,sport,dport,proto_name)
+        
+            if flow_key in flows :
+                flows[flow_key]['packets']+=1
+                flows[flow_key]['last_time']=timestamp
+            else:
+                flows[flow_key]={
+                    'packets':1,
+                    'start_time':timestamp,
+                    'last_time':timestamp
+                }
+            print(f"flow{flow_key}") 
+            print(f"totale pakctes in this flow:{flows[flow_key]["packets"]}")   
+sniff(iface=interface_name , prn=filter_packet , count=100)
+
+print("\n"+"="*10)
+print("all flow")
+for flow, stats in flows.items():
+    print(f"\nFlow: {flow}")
+    print(f"  Packets: {stats['packets']}")
+    print(f"  Duration: {stats['last_time'] - stats['start_time']:.3f} seconds")
