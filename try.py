7fe3f9e45c3561d290f1b9162d6adafd767bf265
turboxox <ismail.saadr@gmail.com>
2026-01-29 05:52:54 +0100

    adding the function the delet the flow after 5 sec

--- try.py
+++ try.py
@@ -6,12 +6,23 @@ flows ={}
 flow_timeout = 5
 interface_name="Intel(R) Wi-Fi 6 AX201 160MHz"
 def filter_packet(pkt):
-    timestamp = pkt.time   
+    timestamp = pkt.time  
+    expired_flows = []
+
+    for flow_key, flow_data in flows.items():
+        if timestamp - flow_data['last_time'] > flow_timeout:
+            expired_flows.append(flow_key)
+
+    for flow_key in expired_flows:
+        print(f"[flow ended bc it passed 5 sec] {flow_key}")  
+        del flows[flow_key]
+
     if pkt.haslayer('IP'):  
         src=pkt['IP'].src
         dst =pkt['IP'].dst
         protocol= pkt['IP'].proto
         tipl =pkt['IP'].len
+    
 
         if pkt.haslayer('UDP'): 
             sport =pkt['UDP'].sport
@@ -30,8 +41,8 @@ def filter_packet(pkt):
                 }
             print(f"flow{flow_key}") 
             print(f"totale pakctes in this flow:{flows[flow_key]["packets"]}")   
-sniff(iface=interface_name , prn=filter_packet , count=100)
 
+sniff(iface=interface_name , prn=filter_packet , count=100)
 print("\n"+"="*10)
 print("all flow")
 for flow, stats in flows.items():
